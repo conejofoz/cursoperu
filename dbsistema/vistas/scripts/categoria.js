2b8e88eaca8b8524ba76299ac8f0e3ac5681a823
geral <geral@192.168.217.131>
2017-09-13 14:38:04 -0300

    admin lte

--- dbsistema/vistas/scripts/categoria.js
+++ dbsistema/vistas/scripts/categoria.js
@@ -6,8 +6,8 @@ var tabla;
 function init() {
     mostrarform(false);
     listar();
-    
-    $("#formulario").on("submit", function(e){
+
+    $("#formulario").on("submit", function (e) {
         guardaryeditar(e);
     })
 }
@@ -63,33 +63,69 @@ function listar() {
                         console.log(e.responseText);
                     }
                 },
-                "bDestroy":true,
-                "iDisplayLength":5, //paginacion
-                "order":[[0, "desc"]]
+        "bDestroy": true,
+        "iDisplayLength": 5, //paginacion
+        "order": [[0, "desc"]]
     }).DataTable();
 }
 
 
 
-function guardaryeditar(e){
+function guardaryeditar(e) {
     e.preventDefault();
     $("#btnGuardar").prop("disabled", true);
     var formData = new FormData($("#formulario")[0]);
-    
+
     $.ajax({
         url: "../ajax/categoria.php?op=guardaryeditar",
         type: "POST",
         data: formData,
         contentType: false,
         processData: false,
-        
-        success: function(datos){
+
+        success: function (datos) {
             bootbox.alert(datos);
             mostrarform(false);
             tabla.ajax.reload();
         }
     });
     limpiar();
+}
+
+
+
+function mostrar(idcategoria){
+    $.post("../ajax/categoria.php?op=mostrar",{idcategoria:idcategoria},function(data, status){
+       data = JSON.parse(data);
+       mostrarform(true);
+       
+       $("#nombre").val(data.nombre);
+       $("#descripcion").val(data.descripcion);
+       $("#idcategoria").val(data.idcategoria);
+    })
+}
+
+
+
+function desactivar(idcategoria){
+    bootbox.confirm("Deseja desactivar la Categoria?", function(result){
+        if(result){
+           $.post("../ajax/categoria.php?op=desactivar",{idcategoria:idcategoria},function(e){
+              bootbox.alert(e);
+              tabla.ajax.reload();
+           });
+        }
+    })
+}
+function activar(idcategoria){
+    bootbox.confirm("Deseja activar la Categoria?", function(result){
+        if(result){
+           $.post("../ajax/categoria.php?op=activar",{idcategoria:idcategoria},function(e){
+              bootbox.alert(e);
+              tabla.ajax.reload();
+           });
+        }
+    })
 }
 
 
