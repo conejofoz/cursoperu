ae5bf66cb1797c6bcab64a8dc20e8ad43d52660e
geral <geral@192.168.217.131>
2017-10-24 09:45:25 -0200

    modificado header

--- dbsistema/vistas/header_1.php
+++ dbsistema/vistas/header_1.php
@@ -38,7 +38,7 @@
       <header class="main-header">
 
         <!-- Logo -->
-        <a href="www.infinity-group.net" class="logo">
+        <a href="index2.html" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>Guri</b>Web</span>
           <!-- logo for regular state and mobile devices -->
@@ -97,7 +97,7 @@
             <?php
             if($_SESSION['escritorio'] == 1){
                 echo '<li>
-              <a href="escritorio.php">
+              <a href="#">
                 <i class="fa fa-tasks"></i> <span>Escritorio</span>
               </a>
             </li> ';
